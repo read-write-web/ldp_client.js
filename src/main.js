744c806d6425c2e458641d335c601063ce58fbaa
Antonio Garrote <antoniogarrote@gmail.com>
2014-07-02 12:58:57 +0100

    Rewriting example code using Async

--- src/main.js
+++ src/main.js
@@ -11,61 +11,122 @@
     /**
      * This code is just for quick testing the server.
      */
-    require(["ldp_client"], function(LDPClient) {
+    require(["ldp_client", "async"], function(LDPClient, async) {
         var container = new LDPClient.LDPResource("/");
-        container.get(function(err, container){
-            if(err){
-                console.log("===================================================");
-                console.log("** ERROR: ");
-                console.log(err);
-            } else {
-                console.log("===================================================");
-                console.log("** success: ");
-                console.log(container.url);
+        var fooResource = null;
 
-                var foo = new LDPClient.LDPResource();
-                foo.representation = "<> a <foaf:Person>; <foaf:name> \"foo\" .";
+        async.series([
 
-                foo.post(container.url, function(err, foo){
-                    if(err) {
+            // Get the LDP container resource
+            function (cb) {
+                container.get(function (err, cont) {
+                    if (err) {
                         console.log("===================================================");
-                        console.log("** ERROR: ");
+                        console.log("** ERROR GET container: ");
                         console.log(err);
+                        cb(err);
                     } else {
+                        container = cont;
                         console.log("===================================================");
-                        console.log("** success: ");
-                        console.log(foo.url);
-
-                        foo.representation = "<> a <foaf:Person>; <foaf:name> \"foo2\" .";
-
-                        foo.put(function(err, foo){
-                            if(err) {
-                                console.log("===================================================");
-                                console.log("** ERROR: ");
-                                console.log(err);
-                            } else {
-                                console.log("===================================================");
-                                console.log("** success: ");
-                                console.log(foo.representation);
-                            }
-
-                            foo.delete(function(){
-                                if(err) {
-                                    console.log("===================================================");
-                                    console.log("** ERROR: ");
-                                    console.log(err);
-                                } else {
-                                    console.log("===================================================");
-                                    console.log("** success: ");
-                                    console.log(foo.url);
-                                }
-                            })
-                        });
+                        console.log("** success GET container: ");
+                        console.log(container.url);
+                        cb();
                     }
                 });
-            }
-        });
+            },
 
+            // Parse the RDF representation of the resource
+            function(cb) {
+                container.parse(function(err){
+                    if (err) {
+                        console.log("===================================================");
+                        console.log("** ERROR PARSE container RDF: ");
+                        console.log(err);
+                        cb(err);
+                    } else {
+                        console.log("===================================================");
+                        console.log("** success PARSE container RDF: ");
+                        console.log(container.graph);
+                        cb();
+                    }
+                });
+            },
+
+            // Run a SPARQL query over the resource
+            function(cb) {
+                container.graph.execute("SELECT ?s ?o WHERE { ?s a ?o }", function(success, triples) {
+                    if(!success) {
+                        console.log("===================================================");
+                        console.log("** ERROR SPARQL querying the container: ");
+                        console.log(triples);
+                        cb(false);
+                    } else {
+                        console.log("===================================================");
+                        console.log("** success SPARQL querying the container: ");
+                        for(var i=0; i< triples.length; i++)
+                            console.log("<"+triples[i].s.value+"> a <"+triples[i].o.value+">");
+                        cb();
+                    }
+                });
+            },
+
+            // POST a new LDP resource into the container
+            function (cb) {
+                fooResource = new LDPClient.LDPResource();
+                fooResource.representation = "<> a <foaf:Person>; <foaf:name> \"foo\" .";
+
+                fooResource.post(container.url, function (err) {
+                    if (err) {
+                        console.log("===================================================");
+                        console.log("** ERROR POST resource: ");
+                        console.log(err);
+                        cb(err);
+                    } else {
+                        console.log("===================================================");
+                        console.log("** success POST resource: ");
+                        console.log(fooResource.url);
+                        cb();
+                    }
+                });
+            },
+
+            // Modify the LDP resource just created
+            function (cb) {
+                fooResource.representation = "<> a <foaf:Person>; <foaf:name> \"foo2\" .";
+                fooResource.put(function (err) {
+                    if (err) {
+                        console.log("===================================================");
+                        console.log("** ERROR PUT resource: ");
+                        console.log(err);
+                        cb(err);
+                    } else {
+                        console.log("===================================================");
+                        console.log("** success PUT resource: ");
+                        console.log(fooResource.representation);
+                        cb();
+                    }
+                });
+            },
+
+            // Delete the resource
+            function (cb) {
+                fooResource.delete(function (err) {
+                    if (err) {
+                        console.log("===================================================");
+                        console.log("** ERROR DELETE resource: ");
+                        console.log(err);
+                        cb(err);
+                    } else {
+                        console.log("===================================================");
+                        console.log("** success DELETE resource: ");
+                        console.log(fooResource.url);
+                        cb();
+                    }
+
+                });
+            }
+
+        ]);
     });
 
 }).call(this);
