346b20250d31e832987c9ec8a38d68cbfabab5b1
Antonio Garrote <antoniogarrote@gmail.com>
2014-07-08 22:18:31 +0100

    Adding additional tests to test suite

--- tests/spec/ldp_client/networking_spec.js
+++ tests/spec/ldp_client/networking_spec.js
@@ -2,13 +2,13 @@
  * Created by antonio on 08/07/2014.
  */
 
-describe("ldp::networking", function(){
+describe("ldp_client/networking", function(){
 
     describe("LDPResource", function(){
 
-        define(["ldp_client/networking", "async"], function(Networking, async) {
+        define(["ldp_client/networking", "async", "underscore"], function(Networking, async, _) {
 
-            it("should be able to discover a remote resource", function (done) {
+            it("should be able to retrieve meta-data for a remote resource", function (done) {
                 Networking.LDPResource.metadata("/", function(err, metadata){
                     expect(err).toEqual(false);
                     expect(metadata.type).toEqual("http://www.w3.org/ns/ldp#BasicContainer");
@@ -103,6 +103,202 @@ describe("ldp::networking", function(){
                     expect(err).toBeFalsy();
                     done();
                 })
+            });
+
+            it("should be possible to create and modify resources in a container", function(done){
+                var container,fooResource, fooResourceUrl;
+               async.series([
+                   // Get the LDP container resource
+                   function (cb) {
+                       container = new Networking.LDPResource("/");
+                       container.get(function (err, cont) {
+                           expect(err).toEqual(false);
+                           expect(container.url).toEqual("/");
+                           if (err) {
+                               console.log("===================================================");
+                               console.log("** ERROR GET container: ");
+                               console.log(err);
+                               cb(err);
+                           } else {
+                               container = cont;
+                               console.log("===================================================");
+                               console.log("** SUCCESS GET container: ");
+                               console.log(container.url);
+                               cb();
+                           }
+                       });
+                   },
+                   // POST a new LDP resource into the container
+                   function (cb) {
+                       fooResource = new Networking.LDPResource();
+                       fooResource.representation = "<> a <http://xmlns.com/foaf/0.1/Person>; <http://xmlns.com/foaf/0.1/name> \"foo\" .";
+
+                       fooResource.post(container.url, function (err) {
+                           expect(err).toBeFalsy();
+                           expect(fooResource.url).not.toBeNull();
+                           if (err) {
+                               console.log("===================================================");
+                               console.log("** ERROR POST resource: ");
+                               console.log(err);
+                               cb(err);
+                           } else {
+                               console.log("===================================================");
+                               console.log("** SUCCESS POST resource: ");
+                               console.log(fooResource.url);
+                               cb();
+                           }
+                       });
+                   },
+
+                   // Modify the LDP resource just created
+                   function (cb) {
+                       fooResource.representation = "<> a <http://xmlns.com/foaf/0.1/Person>; <http://xmlns.com/foaf/0.1/name> \"foo2\" .";
+                       fooResource.put(function (err) {
+                           expect(err).toBeFalsy();
+                           expect(fooResource.representation).not.toBeNull();
+
+                           if (err) {
+                               console.log("===================================================");
+                               console.log("** ERROR PUT resource: ");
+                               console.log(err);
+                               cb(err);
+                           } else {
+                               console.log("===================================================");
+                               console.log("** SUCCESS PUT resource: ");
+                               console.log(fooResource.representation);
+                               fooResource.rel("http://xmlns.com/foaf/0.1/name", function(err,res){
+                                   expect(res.length).toEqual(1);
+                                   expect(res[0]).toEqual('foo2');
+                                   cb();
+                               })
+                           }
+                       });
+                   },
+
+
+                   // Get the resource again
+                   function (cb) {
+                       fooResource.get(function (err, res) {
+                           expect(err).toBeFalsy();
+                           fooResource = res;
+                           if (err) {
+                               console.log("===================================================");
+                               console.log("** ERROR GET resource again: ");
+                               console.log(err);
+                               cb(err);
+                           } else {
+                               console.log("===================================================");
+                               console.log("** SUCCESS GET resource again: ");
+                               console.log(fooResource.representation);
+                               cb();
+                           }
+                       });
+                   },
+
+                   // Delete the resource
+                   function (cb) {
+                       fooResourceURL = fooResource.url;
+                       fooResource.delete(function (err) {
+                           expect(err).toBeFalsy();
+                           if (err) {
+                               console.log("===================================================");
+                               console.log("** ERROR DELETE resource: ");
+                               console.log(err);
+                               cb(err);
+                           } else {
+                               console.log("===================================================");
+                               console.log("** SUCCESS DELETE resource: ");
+                               console.log(fooResource.url);
+                               cb();
+                           }
+
+                       });
+                   },
+
+                   // The resource should not be available anymore
+                   function (cb) {
+                       fooResource = new Networking.LDPResource(fooResourceURL);
+                       fooResource.get(function (err, res) {
+                           fooResource = res;
+                           expect(err).toEqual(true);
+                           expect(res).toEqual(404);
+                           if (err) {
+                               console.log("===================================================");
+                               console.log("** SUCCESS GET removed resource failed with code: ");
+                               console.log(res);
+                               cb();
+                           } else {
+                               console.log("===================================================");
+                               console.log("** ERROR GET removed resource succeeded: ");
+                               console.log(fooResource.representation);
+                               cb(true);
+                           }
+                       });
+                   }
+               ], function(err){
+                   expect(err).toBeFalsy();
+                   done();
+               });
+            });
+
+            it("should be possible to discover remote resources with the right type", function(done){
+
+               async.series([
+                   // Discovering remote LDP resources
+                   function(cb) {
+                       var operations = [];
+
+                       operations.push(function(cb){
+                           Networking.LDPResource.discover("/",function(err, resource){
+                               if(err) {
+                                   cb(true,null);
+                               } else {
+                                   cb(null,resource);
+                               }
+                           })
+                       });
+
+                       operations.push(function(cb){
+                           Networking.LDPResource.discover("/card",function(err, resource){
+                               if(err) {
+                                   cb(true,null);
+                               } else {
+                                   cb(null,resource);
+                               }
+                           });
+                       });
+
+                       async.parallel(
+                           operations,
+                           function(err, results){
+                               _.each(results, function(result){
+                                  if(result.url === "/") {
+                                      expect(result.constructor.name).toEqual("LDPBasicContainer");
+                                   } else if(result.url === "/card") {
+                                      expect(result.constructor.name).toEqual("LDPResource");
+                                   } else {
+                                      expect(true).toBeFalsy();
+                                  }
+                               });
+                               if (err) {
+                                   console.log("===================================================");
+                                   console.log("** ERROR discovering resources: ");
+                                   console.log(res);
+                                   cb(err);
+                               } else {
+                                   console.log("===================================================");
+                                   console.log("** SUCCESS discovering resources: ");
+                                   console.log(results[0].url +" => "+ results[0].constructor.name);
+                                   console.log(results[1].url +" => "+results[1].constructor.name);
+                                   cb();
+                               }
+                           }
+                       );
+                   }
+               ], function(err){
+                   expect(err).toBeFalsy();
+                   done();
+               })
             });
         });
 
